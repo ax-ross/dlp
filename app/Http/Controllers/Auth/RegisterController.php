90b5e65ba94e8a44de9cd897181ab64abb40f4a3
ax-ross <ax-ross@yandex.ru>
2022-10-25 12:00:06 +0300

    fixed saving password

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,12 +7,15 @@ use App\Http\Requests\Auth\RegisterRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function register(RegisterRequest $request)
     {
         $credentials = $request->validated();
+
+        $credentials['password'] = Hash::make($credentials['password']);
 
         $user = User::create($credentials);
         event(new Registered($user));
