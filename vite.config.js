be8646e993dfa9044afdb51801ba7bf69cbf2a2d
ax-ross <ax-ross@yandex.ru>
2022-11-02 18:15:07 +0300

    rmd hmr from vite config

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,6 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    server: {
-        host: '0.0.0.0',
-        hmr: {
-            host: '127.0.0.1'
-        }
-    },
     plugins: [
         laravel({
             input: ['resources/js/app.js', 'resources/js/app.css'],
